78c3622250357f65fcad1fb436aff77305eb37b3
Jesus Contreras <jesus_contreras@brown.edu>
2017-04-20 16:49:52 -0400

    j

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
 function validateEmail() {
 	var email = $('#email-field').val();
-	var regex = /^.*@bronw.edu$/;
+	var regex = /^.*@.*\.edu$/;
 	if(email.match(regex)) {
 		$('#email-error').html("");
 		return true;
