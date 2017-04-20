61482a3ec208d381e4b611e1a14eaba84ad239eb
Luke Priebe <lucas_priebe@brown.edu>
2017-04-20 16:46:37 -0400

    Signup testing

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get('/adminlogin', function(req, res) {
 });
 
 app.post('/signup', function(req, res) {
-  console.log("???");
+  res.send("???");
   Parse.Cloud.run('signUp', req).then(function(signupRes) {
     console.log(signupRes);
 	res.redirect('https://brown.co1.qualtrics.com/jfe/form/SV_6KeyGldHYVWIKln');
