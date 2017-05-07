b724516d4d751cf1b633d2206db33c55b022526e
Luke Priebe <lucas_priebe@brown.edu>
2017-05-07 10:01:25 -0400

    overlay test

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 	drawChart(document.getElementById("consentChart"), consentData);
 	var mainSurveyData = [{name: "complete", count: 55, color: "#6DB1A5"}, {name: "incomplete", count: 30, color: "#4A514B"}];
 	drawChart(document.getElementById("mainSurveyChart"), mainSurveyData);
+	
+	$('.reminder-button').click(openOverlay);
 });
 
 function drawChart(canvas, values) {
@@ -34,4 +36,12 @@ function drawChart(canvas, values) {
 	ctx.arc(125, 125, radius, 0, 2.0*Math.PI);
 	ctx.stroke();
 	ctx.closePath();
+}
+
+function openOverlay() {
+	$('#overlay').css("display", "block");
+}
+
+function closeOverlay() {
+	$('#overlay').css("display", "none");
 }
